8c69a067373d8751172e569e1c5abe7123400a73
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 21:49:50 -0500

    feat: Update home page with pagination and loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,43 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getAllCharacters = async () => {
-      const response = await fetch('https://dragonball-api.com/api/characters');
+      setIsLoading(true);
+      const response = await fetch(`https://dragonball-api.com/api/characters?page=${page}`);
       const data = await response.json();
       setCharacters(data.items);
+      setIsLoading(false);
     };
 
     getAllCharacters();
-  }, []);
+  }, [page]);
 
   return (
     <section>
       <h1>
         Dragonball API
       </h1>
-      <ul>
-        {characters.map((character) => (
-          <li key={character.id}>
-            {character.name}
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {characters.map((character) => (
+            <li key={character.id}>
+              {character.name}
+            </li>
+          ))}
+        </ul>
+      )}
+      <button onClick={() => setPage((page) => page - 1)}>
+        Previous page
+      </button>
+      <button onClick={() => setPage((page) => page + 1)}>
+        Next page
+      </button>
     </section>
   )
 }

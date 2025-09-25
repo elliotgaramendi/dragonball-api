f178b075a5648492baa068b72a75e08408d24105
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-24 21:39:42 -0500

    feat: Add fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getAllCharacters = async () => {
+      const response = await fetch('https://dragonball-api.com/api/characters');
+      const data = await response.json();
+      setCharacters(data.items);
+    };
+
+    getAllCharacters();
+  }, []);
+
   return (
-    <h1>
-      Dragonball API
-    </h1>
+    <section>
+      <h1>
+        Dragonball API
+      </h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>
+            {character.name}
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
